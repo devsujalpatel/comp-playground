c3d55c47890f34d6bee99992d3916d4a48e7fd12
Sujal Patel <sujal7455@gmail.com>
2026-02-01 23:42:37 +0530

    new comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { MorphicNavbar } from "@/components/morphic-navbar";
+import { IconGrid } from "@/components/icon-grid";
 import { ToggleThemeBtn } from "@/components/toggle-theme-btn";
 
 export default function Page() {
   return (
     <div className="flex min-h-screen justify-center items-center bg-neutral-50 dark:bg-neutral-950">
       <ToggleThemeBtn className="absolute top-5 right-5" />
-      <MorphicNavbar />
+      <IconGrid />
     </div>
   );
 }
---
+++ src/components/icon-grid.tsx
@@ -0,0 +1,10 @@
+export const IconGrid = () => {
+  return (
+    <div className="">
+
+    </div>
+  );
+};
+
+
+// https://codersgyan.com/icons/icon-angular.svg
