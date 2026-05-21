df9e7e1208152eef9716e1869dd2cd59f7fe0f2b
Sujal Patel <sujal7455@gmail.com>
2026-05-21 23:16:21 +0530

    new togglebtn and scales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import { AceBento } from "@/components/ace-bento";
-import { ToggleThemeBtn } from "@/components/toggle-theme-btn";
+import { ToggleTheme } from "@/components/toggle-theme-btn";
+import { cn } from "@/lib/utils";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen justify-center items-center bg-white dark:bg-neutral-950">
-      <ToggleThemeBtn className="absolute top-5 right-5" />
-      <AceBento />
+    <div className="flex min-h-screen justify-center items-center bg-gray-100 dark:bg-neutral-900 [--pattern-color:var(--color-red-500)] dark:[--pattern-color:var(--color-green-500)]">
+      <ToggleTheme className="absolute top-5 right-5" />
+      <div
+        className={cn(
+          "size-80 rounded-md shadow-sm shadow-black/10 ring-1 ring-black/10 flex items-center justify-center bg-white dark:bg-neutral-800 dark:shadow-white dark:ring-white",
+          "bg-[repeating-linear-gradient(45deg,var(--pattern-color)_0,var(--pattern-color)_1px,transparent_0,transparent_50%)]",
+          "bg-size-[10px_10px]",
+        )}
+      ></div>
     </div>
   );
 }
--- src/components/toggle-theme-btn.tsx
+++ src/components/toggle-theme-btn.tsx
@@ -1,18 +1,62 @@
 "use client";
 import { useTheme } from "next-themes";
-import { Button } from "./ui/button";
-import { MoonIcon, SunIcon } from "lucide-react";
+import { motion } from "motion/react";
+import { SunIcon, MoonIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
-export const ToggleThemeBtn = ({className}: {className?: string}) => {
-  const { theme, setTheme } = useTheme();
+export const ToggleTheme = ({ className }: { className?: string }) => {
+  const { resolvedTheme, setTheme } = useTheme();
+
+
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div
+        className={cn("flex items-center justify-center h-8 w-8", className)}
+      />
+    );
+  }
 
   const handleClick = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
+    setTheme(isDark ? "light" : "dark");
   };
 
+  const isDark = resolvedTheme === "dark";
+
   return (
-    <Button onClick={handleClick} className={className}>
-      {theme === "dark" ? <SunIcon /> : <MoonIcon />}
-    </Button>
+    <motion.button
+      whileTap={{ scale: 0.9 }}
+      whileHover={{ scale: 1.06 }}
+      className={cn(
+        "flex items-center justify-center h-8 w-8 relative cursor-pointer rounded-md hover:shadow-[inset_-12px_-8px_40px_#46464620] transition-shadow duration-150",
+        className,
+      )}
+      onClick={handleClick}
+    >
+      <SunIcon
+        className={cn(
+          "absolute size-4.5 text-muted-foreground top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300",
+          isDark
+            ? "opacity-100 scale-100 rotate-0"
+            : "opacity-0 scale-0 rotate-90",
+        )}
+      />
+
+      <MoonIcon
+        className={cn(
+          "absolute size-4.5 text-muted-foreground top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300",
+          isDark
+            ? "opacity-0 scale-0 rotate-90"
+            : "opacity-100 scale-100 rotate-0",
+        )}
+      />
+    </motion.button>
   );
 };
